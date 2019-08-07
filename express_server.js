836692ff183bf7ec85353f0f48f80521d06230ec
Lucas <ljwillow@hotmail.com>
2019-08-07 17:01:21 +0000

    fixed random generated shortURL and new input

--- express_server.js
+++ express_server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let result = ' ';
+  let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return result;
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -24,6 +36,18 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let newRandomGeneratedString = generateRandomString();
+  console.log(newRandomGeneratedString);
+  urlDatabase[newRandomGeneratedString] = req.body.longURL;
+  console.log(urlDatabase);  // Log the POST request body to the console
+  res.redirect("/urls/" + newRandomGeneratedString);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
