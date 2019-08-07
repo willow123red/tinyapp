4427e72c818684aec59b90fdd0557a3d41d26dc6
Lucas <ljwillow@hotmail.com>
2019-08-07 00:11:30 +0000

    rendered information about URLs

--- express_server.js
+++ express_server.js
@@ -21,6 +21,18 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars)
+})
+
+
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
